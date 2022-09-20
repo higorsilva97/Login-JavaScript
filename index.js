b184df3f64a7ac291c7546971038ee94dd45f6e3
Higor Silva <higorsilva97@gmail.com>
2022-09-20 02:12:48 -0300

    Cadastro e Login com JSON
    Função Cadastrar e Login utilizando o LocalStorege e JSON

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ let botaoLogin = document.querySelector('.botaoLogin');
 
 usuario.addEventListener('keyup', () => {
     if (usuario.value.length <= 2){
-        usuario.setAttribute('style', 'color:red');
+        usuario.setAttribute('style', 'color:red'); //validação se o usuario ja tiver cadastrado????? ideia pra fazer
         validUsuario = false;
     }else{
         usuario.setAttribute('style', 'color:green');
@@ -71,15 +71,30 @@ function FazerCadastro(){
 function voltarLogin(){
         divCadastro.style.display = 'none';
         tamanhoDivLogin.style.height = '400px';
-        botaoLogin.style.display = 'block';
-        
+        botaoLogin.style.display = 'block';  
 }
-
 
 function Cadastrar(){
     if(validUsuario && validSenha && validConfirmaSenha && validEmail){
+        let listaUser = JSON.parse(localStorage.getItem('listaUser') || '[]');
+
+        listaUser.push(
+            {
+                usuarioCad: usuario.value,
+                senhaCad: senha.value,
+                emailCad: email.value
+            }
+
+        )
+        localStorage.setItem('listaUser', JSON.stringify(listaUser));
+
+        setTimeout(() => {
+            //window.location.href = 'http://127.0.0.1:5500/CRUD-JavaScript/bem-vindo.html'
+            voltarLogin();
+        }, 1500)
+
         msgSucesso.setAttribute('style', 'display: block');
-        msgSucesso.innerHTML = '<strong> Cadastrando usuário </strong>';
+        msgSucesso.innerHTML = '<strong> Cadastrado com sucesso!  </strong>';
         msgErro.setAttribute('style', 'display: none');
         msgErro.innerHTML = '';
         console.log('bom');
@@ -107,14 +122,38 @@ db.transaction(function(criar){
     criar.executeSql("CREATE TABLE users (id INTEGER PRIMARY KEY, usuario TEXT, senha TEXT, email TEXT)");
 });
 
-
-
-
-
+function BuscaUsuario(){
+    for(var i =0; i < localStorage.length; i++){
+        a = localStorage.getItem(localStorage.key(i));
+      }
+      obj = JSON.parse(a);
+      console.log(obj);
+}
 
 function Entrar(){
-
-
+    BuscaUsuario();
+    i = 0;
+    while (i < obj.length)
+    {
+    if(obj[i].usuarioCad === usuario.value && obj[i].senhaCad === senha.value)
+    {
+        msgSucesso.setAttribute('style', 'display: block');
+        msgSucesso.innerHTML = '<strong> Login realizado com sucesso!  </strong>';
+        msgErro.setAttribute('style', 'display: none');
+        msgErro.innerHTML = '';
+        console.log(i);
+        console.log(obj[i].usuarioCad);
+        console.log(obj[i].senhaCad);
+        i = obj.length;
+        
+    }else{  
+        i++;
+        msgErro.setAttribute('style', 'display: block');
+        msgErro.innerHTML = '<strong> Usuario ou senha incorretos! </strong>';
+        msgSucesso.setAttribute('style', 'display: none');
+        msgSucesso.innerHTML = '';
+        }
+      }   
 }
 
 
@@ -127,3 +166,13 @@ function Alterar(){
 
 }
 
+//console.log(localStorage.key(0))
+
+
+
+
+  
+  
+ 
+
+  
